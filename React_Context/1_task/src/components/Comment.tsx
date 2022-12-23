034077c63e8358e0cd393ac1a0d1a5395bb3d142
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2022-12-23 11:09:23 +0100

    React Context Task1 implement upvote functionality

--- React_Context/1_task/src/components/Comment.tsx
+++ React_Context/1_task/src/components/Comment.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useComments } from '../context/CommentsContext'
+import './Comment.css'
 
 export type CommentType = {
     id: string,
@@ -10,22 +11,49 @@ export type CommentType = {
 
 const Comment = () => {
 
-    const {comments} = useComments()
-    console.log(comments)
+    const { comments, setComments } = useComments()
 
-    if(!comments) return (<div>Looking for comments</div>)
+    const handleVotes = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+
+        const id = e.currentTarget.id
+
+        const isUpvote =  e.currentTarget.value === '+'  
+        
+        const newComments: CommentType[] | undefined =  comments?.map(comment => {
+
+            if(comment.id === id ) {
+
+                const newVotes = isUpvote ? comment.votes + 1 : comment.votes - 1
+                
+                return {...comment, votes: newVotes}
+            }
+
+            return comment
+        })
+        if(newComments) setComments(newComments)
+        
+    }
+
+    if (!comments) return (<div>Looking for comments</div>)
+
     return (
         <>
-        {
-            comments.map(comment => 
-                <div id={comment.id}>
-                    <div>{comment.author}</div>
-                    <div>{comment.comment}</div>
-                    <div>{comment.votes}</div>
-                </div>
-                
-            )
-        }
+            {
+                comments.map(comment =>
+                    <div className='comment_container' id={comment.id}>
+                        <div className="comment_votes">
+                            <input id={comment.id} type="button" value="+" onClick={handleVotes} />
+                            <div>{comment.votes}</div>
+                            <input id={comment.id} type="button" value="-" onClick={handleVotes}/>
+                        </div>
+                        <div className="comment_data">
+                            <div>{comment.author}</div>
+                            <div>{comment.comment}</div>
+                        </div>
+                    </div>
+
+                )
+            }
         </>
     )
 }
